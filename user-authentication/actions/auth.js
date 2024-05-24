d07703e7d9247afa0b567f54d4a22de08c4f7cb3
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-05-24 20:56:15 +0500

    Creating user in the database

---
+++ user-authentication/actions/auth.js
@@ -0,0 +1,43 @@
+"use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email || !email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+  if (password.trim().length < 4) {
+    errors.password = "Password must be atleast four character long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  //Enrytping password.
+  const hashedPassword = hashUserPassword(password);
+
+  try {
+    //creating User
+    createUser(email, hashedPassword);
+  } catch (error) {
+    // If email is already registered in data base.
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "It seems like an account for the choosen email is already exist.",
+        },
+      };
+    }
+    throw error;
+  }
+  redirect("/training");
+}
