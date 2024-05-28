36294dc72e35fc066dd0da445c5dc88e4952cf2d
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-05-28 21:11:22 +0500

    adding header and main header

--- next-event-fle-based-routing-page-router/pages/events/[eventId].js
+++ next-event-fle-based-routing-page-router/pages/events/[eventId].js
@@ -1,7 +1,35 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import EventSummery from "../../components/event-detail/event-summary";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+
+import { getEventById } from "../../dummy-data";
+
 export default function EventDetailsPage() {
+  const router = useRouter();
+
+  //Extrecting event id from the url.
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No Event Found.</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail page</h1>
-    </div>
+    <Fragment>
+      <EventSummery title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
